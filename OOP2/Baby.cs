7fe5fed5b222d1189daa11e119223ad953c6744a
Kaan Kaya <kaankaya.0707@gmail.com>
2024-07-31 02:04:46 +0300

    Add project files.

---
+++ OOP2/Baby.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+    public class Baby
+    {
+        //field
+        private string _name;
+        private string _surname;
+        private DateTime _birth;
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birth { get; set; }
+        //Default Constructor
+        //does not take parameters
+        public Baby() 
+        {
+            Console.WriteLine("INGAAAAAAAAAAAAAAAA!");
+            DateTime MomentBirth = DateTime.Now;
+            Console.WriteLine($"Doğum Zamanı : {MomentBirth} ");
+        }
+        //only take name and surname parameters
+        public Baby(string name,string surname)
+        {
+            _name = name;
+            _surname = surname;
+            Console.WriteLine("INGAAAAAAAAAAA!");
+            DateTime MomentBirth = DateTime.Now;
+            Console.WriteLine($"Doğum Zamanı : {MomentBirth} ");
+            Console.WriteLine($"Bebeğin İsmi: {name} - Bebeğin Soyadı : {surname} ");
+
+            
+        }
+
+        //Display Method
+        public void DisplayBaby()
+        {
+            Console.WriteLine($"Bebeğin Adı : {Name} - Bebeğin Soyadı: {Surname} ");
+            Console.WriteLine("********************");
+        }
+
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Baby baby = new Baby();
+        baby.Name = "Andrey";
+        baby.Surname = "Poroşen";
+        baby.DisplayBaby();
+
+        Baby baby1 = new Baby("Ali","Haco");
+    }
+}
